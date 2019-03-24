cf865b9e5b0c471d0e135aaeded3db835ba17c0b
0xf0f <0x0meta@gmail.com>
2019-03-24 13:59:43 +1300

    Added type annotations. - For auto-completion purposes, divisible() results will now be treated as if they were the original type

--- _test.py
+++ _test.py
@@ -1,5 +1,10 @@
-from divisible_sequence import divisible
+from divisible_sequence import divisible, divisible_subtypes
 
-print(divisible([1, 2, 3, 4, 5, 6, 7, 8, 9]) / 3)
-print(divisible('12312312') * .5 / 2)
-print(divisible('345234') * 2.5)
+print((divisible([1, 2, 3, 4, 5, 6, 7, 8, 9]) * 1.5))
+print((divisible('12312312') * .5) / 2)
+print(divisible(divisible('345234') / 3) / 2)
+
+print(divisible_subtypes)
+print(type((divisible('345234') * 1).title()))
+
+print(divisible('asdasd').title())
